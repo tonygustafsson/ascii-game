9eccdf7256340daf15bcd40f2161c03ac433c2cd
Tony Gustafsson <mail@tonyg.se>
2016-10-20 20:05:00 +0200

    Add three audio tracks and they go on repeat

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,33 @@
 "use strict";
 
 var audio = {
+    element:  document.getElementById('audio'),
     init: function init () {
-        var audio = document.getElementById('audio');
-        audio.volume = 0.5;
-        audio.play();
-    }
+        audio.element.addEventListener('canplay', audio.play);
+        audio.element.addEventListener('ended', audio.next);
+
+        audio.element.src = audio.tracks[0];
+        audio.element.load();
+    },
+    play: function play () {
+        audio.element.volume = 0.5;
+        audio.element.play();
+    },
+    next: function nextTrack () {
+        var nextAudioIndex = ++audio.currentTrack;
+
+        if (nextAudioIndex > audio.tracks.length - 1) {
+            nextAudioIndex = 0;
+            audio.currentTrack = 0;
+        }
+
+        audio.element.src = audio.tracks[nextAudioIndex];
+        audio.element.load();
+    },
+    currentTrack: 0,
+    tracks: [
+        'mp3/audio1.mp3',
+        'mp3/audio2.mp3',
+        'mp3/audio3.mp3'        
+    ]
 };
